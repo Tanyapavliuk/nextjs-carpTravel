7260e391711762b303b7052412da24d3a335dd3b
Tanyapavliuk <tanyapavlyk1998@gmail.com>
2023-09-26 18:12:43 +0300

    Revert "delete an unnecessary file"
    This reverts commit 7ce7301c49b367f514bb3f1791197b8964dceccd.

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
-import Title from "../../ui/Title";
 import Form from "./Form";
+import LinkContact from "../../ui/LinkContact";
+import Title from "../../ui/Title";
 
 import style from "./Contact.module.css";
 
@@ -14,45 +15,31 @@ const Contact = () => {
           <div className="grid grid-cols-[1fr_81px] gap-x-5 gap-y-6 mt-9 md:grid-cols-[187px_81px_50px_52px_71px] md:ml-9 md:mb-16 lg:grid-cols-[1fr_1fr] lg:gap-y-[64px] lg:mt-[70px]">
             <ul className="lg:text-right">
               <li key="1">
-                <a
-                  className="hover:underline underline-offset-4 decoration-1 text-white text-xs font-normal  md:text-base lg:text-lg"
-                  href="tel:+38(098)1234567"
-                >
+                <LinkContact href="tel:+38(098)1234567">
                   +38 (098) 12 34 567
-                </a>
+                </LinkContact>
               </li>
               <li key="2">
-                <a
-                  className="hover:underline underline-offset-4 decoration-1 text-white text-sm font-normal  md:text-base lg:text-lg"
-                  href="tel:+38(073)1234567"
-                >
+                <LinkContact href="tel:+38(073)1234567">
                   +38 (073) 12 34 567
-                </a>
+                </LinkContact>
               </li>
             </ul>
             <p className="text-white text-xs font-extralight lg:text-left leading-6">
               Phone number
             </p>
-            <a
-              className="hover:underline underline-offset-4 decoration-1 text-white text-sm font-normal md:text-base lg:text-lg md:col-start-1 md:row-start-2 lg:text-right"
-              href="mailto:support@carptravel.com"
-            >
+            <LinkContact href="mailto:support@carptravel.com">
               support@carptravel.com
-            </a>
+            </LinkContact>
             <p className="text-white text-xs font-extralight md:text-left md:col-start-2 md:row-start-2 md:flex md:items-center leading-6">
               E-mail
             </p>
             <p className="text-white text-xs font-extralight md:text-left md:col-start-4 lg:md:col-start-1 lg:pt-[60px] lg:text-right leading-6">
               Follow us
             </p>
-            <div className="lg:pt-[60px] ">
+            <div className="lg:pt-[60px] flex flex-col">
               {["facebook", "instagram", "youtube", "tiktok"].map((el, i) => (
-                <p
-                  key={i}
-                  className="text-white text-sm font-normal hover:underline underline-offset-4 decoration-1 md:text-base lg:text-lg "
-                >
-                  {el}
-                </p>
+                <LinkContact key={i}>{el}</LinkContact>
               ))}
             </div>
           </div>
